c19ad38e89efcd5f8b1c29ccb718ce7762f82d75
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-07-06 12:50:21 -0500

    Modal - MaterialUI

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -1,8 +1,45 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import PropTypes from 'prop-types';
 import { ModalContext } from "../context/ModalContext";
 
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+
+function getModalStyle() {
+	const top = 50;
+	const left = 50;
+
+	return {
+		top: `${top}%`,
+		left: `${left}%`,
+		transform: `translate(-${top}%, -${left}%)`,
+	};
+}
+
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		position: "absolute",
+		width: 600,
+		backgroundColor: theme.palette.background.paper,
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3),
+	},
+}));
+
 const Receta = ({receta}) => {
+
+    //Configuración del modal material-UI
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+
+    const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     //Extraer valores del context
     const { setIdreceta }  = useContext(ModalContext);
@@ -19,11 +56,25 @@ const Receta = ({receta}) => {
                         type="button"
                         className="btn btn-block btn-primary"
                         onClick={() => {
-                            setIdreceta(receta.idDrink)
+                            setIdreceta(receta.idDrink);
+                            handleOpen();
                         }}
                     >
                         See Recipe
                     </button>
+
+
+                    <Modal
+                        open={open}
+                        onClose={() => {
+                            setIdreceta(null);
+                            handleClose();
+                        }}
+                    > 
+                        <div style={modalStyle} className={classes.paper}>
+                            <h1>Modal</h1>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -8,6 +8,21 @@ const ModalProvider = (props) => {
 
     //State del provider
     const [idreceta, setIdreceta] = useState(null);
+    const [recetaMod, setRecetaMod] = useState({});
+
+    //Llamado a la API
+    useEffect(() => {
+        const obtenerReceta = async () => {
+            if(!idreceta) return;
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+            const resultado = await axios(url);
+
+            setRecetaMod(resultado.data.drinks[0]);
+        }
+        obtenerReceta();
+
+    }, [idreceta])
 
     return ( 
         <ModalContext.Provider
