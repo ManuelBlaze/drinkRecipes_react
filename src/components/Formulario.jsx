5a92f1bf181af7c14b51c2c7261875ad88bafbf5
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-07-06 10:51:30 -0500

    RecetasContext API

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,32 +1,54 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import { CategoriasContext } from '../context/CategoriasContext';
+import { RecetasContext } from '../context/RecetasContext';
 
 const Formulario = () => {
 
+    const [busqueda, setBusqueda] = useState({
+        name: '',
+        category: ''
+    })
+
     const { categorias } = useContext(CategoriasContext);
+    const { setBusquedaRecetas, setConsultar } = useContext(RecetasContext);
+
+    //Leer el contenido de seleccion
+    const obtenerDatos = e => {
+        setBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        })
+    }
 
     return (
         <form
             className="col-12"
+            onSubmit={e => {
+                e.preventDefault();
+                setBusquedaRecetas(busqueda);
+                setConsultar(true);
+            }}
         >
             <fieldset className="text-center">
                 <legend>Search drinks by Category or Ingredients</legend>
             </fieldset>
 
-            <div className="row">
-                <div className="col-md-4 mt-4">
+            <div className="row mt-4">
+                <div className="col-md-4">
                     <input 
                         type="text" 
                         name="name" 
                         className="form-control"
                         placeholder="Search by Ingredient"
-                    />
+                        onChange={obtenerDatos}
+                        />
                 </div>
-                <div className="col-md-4 mt-4">
+                <div className="col-md-4">
                     <select 
                         className="form-control"
                         name="category" 
+                        onChange={obtenerDatos}
                     >
                         <option value="">-- Select Category --</option>
                         {categorias.map( categoria => (
@@ -37,7 +59,7 @@ const Formulario = () => {
                         ))}
                     </select>
                 </div>
-                <div className="col-md-4 mt-4">
+                <div className="col-md-4">
                     <input 
                         type="submit" 
                         value="Search Drink" 
---
+++ src/context/RecetasContext.jsx
@@ -0,0 +1,42 @@
+import React, { useState, createContext, useEffect } from "react";
+import axios from "axios";
+
+export const RecetasContext = createContext();
+
+const RecetasProvider = (props) => {
+
+    const [recetas, setRecetas] = useState([])
+    const [busquedaRecetas, setBusquedaRecetas] = useState({
+        name: '',
+        category: ''
+    });
+    const [consultar, setConsultar] = useState(false);
+
+    const { name, category } = busquedaRecetas;
+
+    useEffect(() => {
+        if (consultar) {
+            const obtenerRecetas = async () => {
+                const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}&c=${category}`;
+                const recetas = await axios(url);
+                
+                setRecetas(recetas.data.drinks);
+            }
+            obtenerRecetas();
+        }
+
+    }, [busquedaRecetas])
+
+    return ( 
+        <RecetasContext.Provider
+            value={{
+                setBusquedaRecetas, 
+                setConsultar
+            }}
+        >
+            {props.children}
+        </RecetasContext.Provider>
+    );
+}
+
+export default RecetasProvider;
